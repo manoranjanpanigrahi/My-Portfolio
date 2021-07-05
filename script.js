f4ffa67a789d91580e287993e22fbbb80b9a0911
manoranjanpanigrahi <manoranjan.panigrahi9@gmail.com>
2021-07-05 10:43:58 +0530

    major changes

--- script.js
+++
@@ -1,9 +0,0 @@
-
-var item = document.getElementById("main");
-
-  window.addEventListener("wheel", function (e) {
-    if (e.deltaY > 0) 
-      item.scrollLeft += 300;
-    else 
-      item.scrollLeft -= 300;
-  });
